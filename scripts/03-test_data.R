d1cf9c6634b7954eeb50ea77774667ba418a5f41
Jayden Jung <83850482+jj-andj@users.noreply.github.com>
2023-03-16 11:29:53 -0400

    Testing the dataset that is actually used in paper

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -13,17 +13,17 @@ library(tidyverse)
 #### Test data ####
 
 ## Test if Happiness has 3 unique values (this includes NA)
-gss2018_cleaned$Happiness |> unique() |> length() >= 3
+merged_gss$Happiness |> unique() |> length() >= 3
 
 ## Test if Highest Educ's has 21 unique values (this includes NA)
-gss2018_cleaned$Highest_Educ_Level |> unique() |> length() >= 20
+merged_gss$Highest_Educ_Level |> unique() |> length() >= 20
 
 ## Test if Occupational Prestige is greater than or equal to 10
-gss2018_cleaned$Occupational_Prestige_Pct |> min(na.rm = TRUE) > 10
+merged_gss$Occupational_Prestige_Pct |> min(na.rm = TRUE) > 10
 
 ## Test if Occupational Prestige is less than 91
-gss2018_cleaned$Occupational_Prestige_Pct |> max(na.rm = TRUE) < 91
+merged_gss$Occupational_Prestige_Pct |> max(na.rm = TRUE) < 91
 
 ## Test if sex is 2 (female)
-gss2018_cleaned$sex |> unique() == 2
+merged_gss$sex |> unique() == 2
 
